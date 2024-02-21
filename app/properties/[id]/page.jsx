e6a9a6f0d0c902d324d78c965c398572a86c000d
MichaelWiss <michael.wiss@gmail.com>
2024-02-21 11:41:58 -0600

    client, routing

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+import { useRouter } from "next/navigation";
+
+const PropertyPage = () => {
+    const router = useRouter();
+
+    console.log('Hello');
+
+    return (<div>
+    <button onClick={() => router.push('/')}
+        className='bg-blue-500 p-2'>Go Home</button>
+    </div>
+    );
+};
+export default PropertyPage;
