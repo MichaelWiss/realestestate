382a20c5a52f2c3f621460ba40d6cb64e7ae16ec
MichaelWiss <michael.wiss@gmail.com>
2024-02-21 18:20:13 -0600

    next router hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,16 +1,18 @@
 'use client';
-import { useRouter, useParams } from "next/navigation";
+import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
 
 const PropertyPage = () => {
     const router = useRouter();
+    const searchParams = useSearchParams();
     const { id } = useParams();
+    const name = searchParams.get('name');
+    const pathname = usePathname();
 
-    console.log('Hello');
 
     return (<div>
     <button onClick={() => router.push('/')}
         className='bg-blue-500 p-2'>
-        Go Home { id }
+        Go Home {pathname}
         </button>
     </div>
     );
