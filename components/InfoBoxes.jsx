897b97329684c4d3dce3161e8e08df70cb83029f
MichaelWiss <michael.wiss@gmail.com>
2024-02-26 17:40:18 -0600

    InfoBox

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -16,6 +16,16 @@ const InfoBoxes = () => {
         }}>
         Find your dream rental property. Bookmark properties and contact owners.
         </InfoBox>
+        <InfoBox
+        heading='For Property Owners'
+        backgroundColor="bg-blue-100"
+        buttonInfo={{
+            text: 'Add Properties',
+            link: '/properties/add',
+            backgroundColor: 'bg-blue-500'
+        }}>
+        List your properties and reach potential tenants.  Rent as an Airbnb or long term.
+        </InfoBox>
       </div>  
     </div>
   </section>
