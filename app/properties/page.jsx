56b3332d7b8777b67a4867266f1a82d83eab8fcc
MichaelWiss <michael.wiss@gmail.com>
2024-02-27 16:11:49 -0600

    PropertyCard

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,18 +1,25 @@
+import PropertyCard from '@/components/PropertyCard';
 import properties from '@/properties.json';
 import React from 'react'
 
 const PropertyPage = () => {
   return (
-    <section class="px-4 py-6">
-    <div class="container-xl lg:container m-auto">
-      <h2 class="text-3xl font-bold text-blue-500 mb-6 text-center">
+    <section className="px-4 py-6">
+    <div className="container-xl lg:container m-auto">
+      <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
         Recent Properties
       </h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             { properties.map((property) => (
-              <div>{ property.name }</div>
+              <PropertyCard key={property._id} 
+                property={property} />
             ))}
         </div>
+          )}
+        
     </div>
     </section>
     
