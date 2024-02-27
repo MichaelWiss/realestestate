108d4bd7d9d70c8ab0b50071a0f390dfcfe11df4
MichaelWiss <michael.wiss@gmail.com>
2024-02-27 14:55:32 -0600

    mapping properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,13 +1,22 @@
-import Link from 'next/link';
+import properties from '@/properties.json';
 import React from 'react'
 
 const PropertyPage = () => {
   return (
-    <div>
-        <h1 className="text-3xl">Properties</h1>
-        <Link href="/">Go Home</Link>
-    </div>
+    <section class="px-4 py-6">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-blue-500 mb-6 text-center">
+        Recent Properties
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+         { properties.map((property) => (
+          <div>{ property.name }</div>
+         ) )}
+      </div>
+      </div>
+      </section>
+    
     );
-}
+};
 
 export default PropertyPage;
