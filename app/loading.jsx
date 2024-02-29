a20e50d58fbc02a5573a35fa4182fa4972c649ed
MichaelWiss <michael.wiss@gmail.com>
2024-02-29 14:38:32 -0600

    spinner

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,23 @@
+'use client';
 import React from 'react'
 
-const LoadingPage = () => {
-  return (
-    <div>Loading...</div>
-  )
-}
+import ClipLoader from "react-spinners/ClipLoader";
 
-export default LoadingPage
+const override = {
+    display: 'block',
+    margin: '100px auto',
+};
+
+const LoadingPage = ({ loading }) => {
+  return (
+    <ClipLoader
+        color='#3b82f6'
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+    />
+  );
+};
+
+export default LoadingPage;
