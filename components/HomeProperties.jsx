5a88f019daf51013233ab835a7392fbe58d7babe
MichaelWiss <michael.wiss@gmail.com>
2024-02-29 13:35:14 -0600

    mapping over recentproperties

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
 
 
 const HomeProperties = () => {
+    const recentProperties = properties
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 3);
+
   return (
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto">
-        <h2 class="text-3xl font-bold text-blue-500 mb-6 text-center">
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
           Recent Properties
         </h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            { recentProperties === 0 ? (
+                <p>No Properties Found</p>
+            ) : recentProperties.map((property) => (
+                <PropertyCard key={property._id} property={ property } />
+            )) }
         </div>
         </div>
         </section>
