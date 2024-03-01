1b6c1134c3115be65b89eb8b792f4519057de913
MichaelWiss <michael.wiss@gmail.com>
2024-03-01 14:00:42 -0600

    connected

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,17 @@
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
+import connectDB from '@/config/database';
 
-function HomePage() {
-  
+const HomePage = async () => {
+  await connectDB();
   return (
     <div>
     <Hero/>
     <InfoBoxes />
     <HomeProperties/>
     </div>
-  )
+  );
 };
 
 export default HomePage;
--- config/database.js
+++ config/database.js
@@ -3,5 +3,22 @@ import mongoose from "mongoose";
 let connected = false;
 
 const connectDB = async () => {
+    mongoose.set('strictQuery', true);
 
+    // if database already connected, don't connect again.
+    if (connected) {
+        console.log('MongoDB is already connected...');
+        return;
+    }
+
+    // connect
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        connected = true;
+        console.log('MongoDB connected...');
+    } catch (error) {
+        console.log(error);
+    }
 };
+
+export default connectDB;
