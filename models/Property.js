d27962f9b311fe10b96421a2f6b9df775920edfe
MichaelWiss <michael.wiss@gmail.com>
2024-03-04 15:05:11 -0600

    Property schema

---
+++ models/Property.js
@@ -0,0 +1,54 @@
+import { Schema, model, models } from 'mongoose';
+
+const PropertySchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+   name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    description: {
+            type: String,
+    },
+    location: {
+        street: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        state: {
+            type: String,
+        },
+        zipcode: {
+            type: String,
+        }
+    },
+    beds: {
+        type: Number,
+        required: true,
+    },
+    baths: {
+        type: Number,
+        required: true,
+    },
+    square_feet: {
+        type: Number,
+        required: true,
+    },
+    amenities: [
+        {
+            type: String
+        }
+    ],
+    rates: {
+        nightly: 
+    }
+});
