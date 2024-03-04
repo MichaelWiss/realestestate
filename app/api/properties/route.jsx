33b049d6f5b09b1b2ae8a6682ea26b4f2fbd23b2
MichaelWiss <michael.wiss@gmail.com>
2024-03-04 12:49:15 -0600

    api/properties

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,7 +1,9 @@
 export const GET = async (request) => {
     try {
-        return new Response('Hello World', status: 200 );
-    }
+        return new Response('Hello World', {status: 200});
 } catch (error) {
-
+    console.log(error);
+    return new Response('Something went wrong', {
+        status: 500 });
+   }
 };
