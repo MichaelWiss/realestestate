a6263c597bcdfe99f82541fb535b9784768c4c08
MichaelWiss <michael.wiss@gmail.com>
2024-03-04 14:37:22 -0600

    User schema

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,6 +1,11 @@
+import connectDB from "@/config/database";
+
+
 export const GET = async (request) => {
     try {
-        return new Response('Hello World', {status: 200});
+        await connectDB();
+
+        return new Response(JSON.stringify({message: 'Hello World'}), {status: 200});
 } catch (error) {
     console.log(error);
     return new Response('Something went wrong', {
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
-import connectDB from '@/config/database';
 
-const HomePage = async () => {
-  await connectDB();
+
+const HomePage = () => {
   return (
     <div>
     <Hero/>
