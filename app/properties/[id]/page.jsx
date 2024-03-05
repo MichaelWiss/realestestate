6775549c577e5ffdf15b86cd1fe933d30e9898a4
MichaelWiss <michael.wiss@gmail.com>
2024-03-05 14:25:08 -0600

    fetchPropertyData

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,29 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
+
 
 const PropertyPage = () => {
-    
+    const { id } = useParams();
+
+    const [property, setProperty] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPropertyData = async () => {
+            if (!id) return;
+            try {
+
+            } catch (error) {
+
+            }
+
+        }
+    }, [id, property]);
+
+
+
     return (<div>
         PropertyPage
     </div>
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+//Fetch all properties
+
 async function fetchProperties() {
     try {
 
@@ -19,4 +21,25 @@ async function fetchProperties() {
     }
   }
 
-  export { fetchProperties };
+  // Fetch single property
+
+  async function fetchProperty(id) {
+    try {
+
+        if (!apiDomain) {
+            return null;
+        }
+
+      const res = await fetch(`${apiDomain}/api/properties/${id}`);
+  
+      if(!res.ok) {
+        throw Error('Failed to fetch data');
+      }
+      return res.json();
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  export { fetchProperties,  fetchProperty};
