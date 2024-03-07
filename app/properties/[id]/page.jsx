da505809a06a26dc6bd133602b795c09b112ed3b
MichaelWiss <michael.wiss@gmail.com>
2024-03-07 15:58:31 -0600

    property page image

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { fetchProperty } from '@/utils/requests';
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import Link from 'next/link';
 
 
 const PropertyPage = () => {
@@ -14,19 +16,45 @@ const PropertyPage = () => {
         const fetchPropertyData = async () => {
             if (!id) return;
             try {
-
+                const property = await fetchProperty(id);
+                setProperty(property);
             } catch (error) {
-
+                console.error('Error fetching property:', error);
+            } finally {
+                setLoading(false);
             }
-
+        }
+        if(property === null) {
+            fetchPropertyData();
         }
     }, [id, property]);
 
-
-
-    return (<div>
-        PropertyPage
-    </div>
+ if (!property && !loading) {
+    return (
+        <h1 classNameName='text-center text-2xl font-bold mt-10'>
+            Property Not Found
+        </h1>
     );
-};
+ }
+
+    return  (
+        <>
+            {!loading && property && (
+            <>
+            <PropertyHeaderImage image={property.images[0]} />
+            <section>
+                <div className="container m-auto py-6 px-6">
+                    <Link
+                        href="/properties.html"
+                        className="text-blue-500 hover:text-blue-600 flex items-center"
+                        >
+                    <i className="fas fa-arrow-left mr-2"></i> Back to Properties
+                    </Link>
+            </div>
+    </section>
+        </>
+        )}
+        </>
+        );
+    };
 export default PropertyPage;
