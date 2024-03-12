b865c9ddbfd7c2b7fe4b6c5c3e1315f2d4f0de4f
MichaelWiss <michael.wiss@gmail.com>
2024-03-12 18:47:08 -0500

    Spinner

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -91,7 +91,7 @@ export default function PropertyDetails({ property }) {
               <h3 className="text-lg font-bold mb-6">Amenities</h3>
 
               <ul
-                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none"
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none space-y-2"
               >
               { property.amenities.map((amenity, index) => (
                 <li key={index}>
