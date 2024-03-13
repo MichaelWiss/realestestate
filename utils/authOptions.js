e989fc74d5bc04bea986d350a6c9cc6067080335
MichaelWiss <michael.wiss@gmail.com>
2024-03-13 18:35:17 -0500

    authOptions route

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -13,5 +13,20 @@ export const authOptions = {
                 }
             }
         })
-    ]
+    ],
+    callbacks: {
+        // invoked on successful signin
+        async signIn({ profile }) {
+            // 1. Connect to db
+            // 2. Check if user exists
+            // 3. If not, then add user to database
+            // 4. Return true to allow sign in
+        }, 
+        // Modifies the session object
+        async session({ session }) {
+            // 1. Get user from database
+            // 2. Assign the user id to the session
+            // 3. return session
+        }
+    }
 };
