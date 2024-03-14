4ba221943eb9d38ee356e9f49bff349ad5ab6ac6
MichaelWiss <michael.wiss@gmail.com>
2024-03-14 18:49:34 -0500

    mobile signIn button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -245,11 +245,15 @@ const Navbar = () => {
             Add Property
           </Link>
           )}
-          {!session && (
-               <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
-            <span>Login or Register</span>
-          </button>
-          )}
+          {!session && providers && 
+                Object.values(providers).map((provider, index) => (
+                <button onClick={() => signIn(provider.id)}
+                   key={index} 
+                   className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                 
+                 <span>Login or Register</span>
+                </button>
+              ))}
          
         </div>
       </div>
