bd4c22df145add69478828c418f184a34808979d
MichaelWiss <michael.wiss@gmail.com>
2024-03-25 17:32:52 -0500

    closing menu

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-    matcher = ['/properties/add', '/profile', '/properties/saved',
+    matcher : ['/properties/add', '/profile', '/properties/saved',
 '/messages'],
 };
