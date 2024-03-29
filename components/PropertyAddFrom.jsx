f8cf7f99acb0585395f40df1df5c07aaf1089acc
MichaelWiss <michael.wiss@gmail.com>
2024-03-29 17:14:53 -0500

    event handler

--- components/PropertyAddFrom.jsx
+++ components/PropertyAddFrom.jsx
@@ -37,7 +37,10 @@ const PropertyAddFrom = () => {
         setMounted(true);
     }, []);
 
-    const handleChange = ()=> {}
+    const handleChange = (e)=> {
+      const { name, value }= e.target;
+    };
+    
     const handleAmenitiesChange = ()=> {}
     const handleImageChange = ()=> {}
 
@@ -50,9 +53,9 @@ const PropertyAddFrom = () => {
             <div className="mb-4">
               <label
                 htmlFor="type"
-                className="block text-gray-700 font-bold mb-2"
-                >Property Type</label
-              >
+                className="block text-gray-700 font-bold mb-2">
+                Property Type
+                </label>
               <select
                 id="type"
                 name="type"
