e9bed50e4ad5a204691a4ce60fd3e262be90dd5d
MichaelWiss <michael.wiss@gmail.com>
2024-04-19 18:03:03 -0500

    propertyData object

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from '@/models/Property';
+import Email from "next-auth/providers/email";
 
 // GET /api/properties
 export const GET = async (request, { params }) => {
@@ -25,6 +26,35 @@ export const POST = async (request) => {
         // Access all values from amenities and images
         const amenities = formData.getAll('amenities');
         const images = formData.getAll('images').filter((image) => image.name !== '');
+
+        // Create propertyData object for database
+        const propertyData = {
+            type: formData.get('type'),
+            name: formData.get('name'),
+            description: formData.get('description'),
+            location: {
+                street: formData.get('location.street'),
+                city: formData.get('location.city'),
+                state: formData.get('location.state'),
+                zipcode: formData.get('location.zipcode'),
+            },
+            beds: formData.get('beds'),
+            name: formData.get('baths'),
+            square_feet: formData.get('square_feet'),
+            amenities,
+            rates: {
+                weekly: formData.get('rates.weekly'),
+                monthly: formData.get('rates.monthly'),
+                nightly: formData.get('rates.nightly'),
+            },
+            seller_info: {
+                name: formData.get('seller_info.name'),
+                email: formData.get('seller_info.email'),
+                phone: formData.get('seller_info.phone'),
+            },
+            images
+        };
+        console.log(propertyData);
 
         console.log(amenities, images);
         
