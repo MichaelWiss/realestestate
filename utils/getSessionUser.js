07aa2ad43a98415b2897900f31417cb4114a2416
MichaelWiss <michael.wiss@gmail.com>
2024-04-29 17:56:39 -0500

    User validation

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -2,5 +2,15 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from "@/utils/authOptions";
 
 export const getSessionUser = async () => {
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        return null;
+    }
+
+    return {
+        user: session.user,
+    };
     
-}
+};
