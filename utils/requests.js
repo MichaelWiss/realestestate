2aca16cbfd4b2917f008f136988e61e8573e112c
MichaelWiss <michael.wiss@gmail.com>
2024-05-28 13:10:59 -0500

    errors

--- utils/requests.js
+++ utils/requests.js
@@ -9,7 +9,8 @@ async function fetchProperties() {
             return [];
         }
 
-      const res = await fetch(`${apiDomain}/api/properties`,);
+      const res = await fetch(`${apiDomain}/api/properties`, {
+        cache: 'no-store'});
   
       if(!res.ok) {
         throw Error('Failed to fetch data');
